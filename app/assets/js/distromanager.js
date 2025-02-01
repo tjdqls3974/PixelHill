28020ebbcc39b82eb3d0acc9cb65771324e744dc
tjdqls3974 <tlstjs3833@gmail.com>
2025-02-01 21:31:42 +0900

    ok

--- app/assets/js/distromanager.js
+++ app/assets/js/distromanager.js
@@ -4,7 +4,7 @@ const ConfigManager = require('./configmanager')
 
 // Old WesterosCraft url.
 // exports.REMOTE_DISTRO_URL = 'http://mc.westeroscraft.com/WesterosCraftLauncher/distribution.json'
-exports.REMOTE_DISTRO_URL = 'https://cdn.mysticred.space/launcher/distribution.json'
+exports.REMOTE_DISTRO_URL = 'https://tjdqls3974.github.io/tjdqls3974/distribution.json'
 
 const api = new DistributionAPI(
     ConfigManager.getLauncherDirectory(),
